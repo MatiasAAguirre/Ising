25795fd643a214d31f405d9ebc0728a5b5a89a7f
Matias Aguirre <aguirrematiasa@live.com.ar>
2019-05-22 16:53:28 -0300

    Subida de nuestro gran avance.

--- 2b.c
+++ 2b.c
@@ -8,14 +8,15 @@
 int poblar (int *red, float p, int dim);
 int contornos(int *red, int dim);
 int conteo(int *red, int dim);
-int flipeo(int *red, int dim, float Be);
+int flipeo(int *red, int dim, float Be, float Jo);
+int vecinos(int *red,int dim, int i, int j);
 int imprimir(int *red,int dim);
 
 
 //Función principal:
 int main(int argc,char*argv[])
-{ float p=0.5, Be=0.01;
-  int dim, i, it=100000000, mag=0;
+{ float p=0.5, Be=0, Jo=0.1;
+  int dim, i, it=1, mag=0;
   int *red;
   FILE *magne;
 
@@ -24,20 +25,19 @@ int main(int argc,char*argv[])
   dim = dim + 2;
 
   red = malloc(dim*dim*sizeof(int));    //Reserva el espacio necesario para la red.
-  magne = fopen("./Magnetizacion/Magne.txt", "a");
+  magne = fopen("./Magnetizacion/MagneB.txt", "a");
   poblar(red, p, dim);
   contornos(red,dim);
+  imprimir(red, dim);
   mag = conteo(red, dim);
   for (i=0; i<it;i++)
-    { mag+=flipeo(red, dim, Be);
+    { mag+=flipeo(red, dim, Be, Jo);
       contornos(red,dim);
       if(i%10==0)
         {
           fprintf(magne, "%i %i\n", i, mag);
         }
     }
-
-  //printf("%i\n", mag);
 
   free(red);
 
@@ -80,24 +80,34 @@ int contornos(int *red, int dim)
     return 0;
   }
 
-int flipeo(int *red, int dim, float Be)
+int flipeo(int *red, int dim, float Be, float Jo)
   { float r;
-    float *P_ind;
+    float *P_ind, *P_int;
     int i, j, c=0;
 
     srand(time(NULL));
 
     P_ind = malloc(2*sizeof(float));
+    P_int = malloc(5*sizeof(float));
 
     *(P_ind) = 1.0/exp(2.0*Be);
     *(P_ind+1) = exp(2.0*Be);
+
+    *(P_int) = exp(-8.0*Jo);
+    *(P_int+1) = exp(-4.0*Jo);
+    *(P_int+2) = 1;
+    *(P_int+3) = exp(4.0*Jo);
+    *(P_int+4) = exp(8.0*Jo);
 
     for (i=1;i<dim-1;i++)
       { for (j=1;j<dim-1;j++)
         { r = ((float)rand())/((float)RAND_MAX);
 
-          if(*(P_ind+((*(red+i*dim+j) + 1)/2))>r)
+          if(((*(P_ind+((*(red+i*dim+j) + 1)/2)))*(*(P_int+vecinos(red, dim, i, j))))>r)
             { *(red+i*dim+j) = - *(red+i*dim+j);
+              printf("%i %i %f\n",i*dim+j, vecinos(red, dim, i, j),  *(P_int+vecinos(red, dim, i, j)));
+              imprimir(red, dim);
+              printf("\n");
               if(*(red+i*dim+j)<0)
                 {
                   c += -2;
@@ -110,6 +120,7 @@ int flipeo(int *red, int dim, float Be)
         }
 
     free(P_ind);
+    free(P_int);
 
     return c;
   }
@@ -124,6 +135,43 @@ int conteo(int *red, int dim)
         }
     }
     return c;
+}
+
+int vecinos(int *red,int dim, int i, int j)
+{ int S1,S2, S3, S4, index;
+
+  S1 = *(red+(i*dim+j-1));   //El casillero de la izquierda.
+  S2 = *(red+(i*dim+j-dim)); //El casillero de arriba.
+  S3 = *(red+(i*dim+j+1));    //El casillero de la derecha.
+  S4 = *(red+(i*dim+j+dim));  //El casillero de la abajo.
+
+  if(S1+S2+S3+S4==-4)
+    {
+      index=0;
+    }
+
+  if(S1+S2+S3+S4==-2)
+    {
+      index=1;
+    }
+
+  if(S1+S2+S3+S4==0)
+    {
+      index=2;
+    }
+
+  if(S1+S2+S3+S4==2)
+    {
+      index=3;
+    }
+
+  if(S1+S2+S3+S4==4)
+    {
+      index=4;
+    }
+
+  return index;
+
 }
 
 int imprimir(int *red, int dim)         //Imprime una fila debajo de la otra.
